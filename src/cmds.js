ff5bfb0f374d6737171a84b0e8d77c7b9c18b8ab
Boris Filipov <obiwonn@gmail.com>
2013-12-14 19:38:57 +0200

    support overriding of default install,uninstall,upgrade commands

--- src/cmds.js
+++ src/cmds.js
@@ -1,20 +1,15 @@
-var _ = require("underscore")
-
 module.exports = function(angel){
-  angel.on("cell :cmd :mode", function(angel, next){
-    if(_.contains(["install", "uninstall", "upgrade"], angel.cmdData.cmd))
-      return next()
-    angel.series([
-      angel.loadCellData,
-      function(angel, next){
-        var commands = "cd {cwd}; . {nvmPath}; nvm use {nodeVersion}; {"+angel.cmdData.cmd+"}"
-        if(angel.cmdData.remote)
-          angel.ssh("remote", commands, next)
-        else
-          angel.exec(commands, next)
-      }
-    ])(angel, next)
-  })
+  angel.on("cell :cmd :mode", angel.series([
+    angel.loadCellData,
+    function(angel, next){
+      if(!angel.cmdData[angel.cmdData.cmd]) return next()
+      var commands = "cd {cwd}; . {nvmPath}; nvm use {nodeVersion}; {"+angel.cmdData.cmd+"}"
+      if(angel.cmdData.remote)
+        angel.ssh("remote", commands, next)
+      else
+        angel.exec(commands, next)
+    }
+  ]))
   .example([
     "$ angel cell {cmd} ./file.json",
   ].join("\n"))
